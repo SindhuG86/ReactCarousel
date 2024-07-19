5ff1016d2874a90389126ca1db525ea3f5cad00e
SindhuG86 <sthiagarajan@agilitysolutions.us>
2024-07-19 10:56:59 -0700

    Add files via upload
    committing changes in App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ShopCarousel from "./Carouselcomponent/ShopCarousel";
+import "./index.css";
+
+const App = () => {
+  return (
+    <div className="App">
+    <h1>Shop Names Carousel</h1>
+      <ShopCarousel />
+    </div>
+  );
+};
+export default App;
